10599a5fbdf147803fcd051dca316e0101e0b3cb
Caven <34606118+Caven15@users.noreply.github.com>
2024-06-12 16:30:00 +0200

    constructeurs

---
+++ [6] constructeur/dist/app.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const livreTechnique_1 = require("./models/livreTechnique");
+// Créations livres
+const livre1 = new livreTechnique_1.LivreTechnique("Le seigneur des anneaux", "Tolkien", undefined, 25, "SF", "NB");
+const livre2 = new livreTechnique_1.LivreTechnique("Administrations & sécurisations des réseaux", "Tintin", 1997, 250, "Informatique", "Expert");
+// Utilisation du polymorphisme pour manipuler les livres via l'interface ILivre
+const livres = [livre1, livre2];
+// Parcourir chaque livre et afficher ses détails
+console.log("--- ILivre ---");
+livres.forEach(livre => {
+    livre.AfficherLivre();
+    console.log(`Prix : ${livre.Prix}`); // ReadOnly
+});
+// Manipulation spécifique aux livres techniques
+// On caste les objets pour utiliser les propriétés et méthodes spécifiques à ILivretechnique
+const LivresTechniques = [livre1, livre2];
+// PArcourir chaque livre technique et afficher les détails spécifiques
+console.log("--- ILivreTechnique ---");
+LivresTechniques.forEach(livreTechnique => {
+    console.log(`Domaine : ${livreTechnique.Domaine}`);
+    console.log(`Niveau : ${livreTechnique.Niveau}`);
+});
---
+++ [6] constructeur/dist/models/livre.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Livre = void 0;
+class Livre {
+    constructor(titre, auteur, anneePublication = new Date().getFullYear(), prix) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.anneePublication = anneePublication;
+        this.prix = prix;
+    }
+    // Prop's
+    get Titre() { return this.titre; }
+    set Titre(titre) { this.titre = titre; }
+    get Auteur() { return this.auteur; }
+    set Auteur(auteur) { this.auteur = auteur; }
+    set AnneePublication(anneePublication) { this.anneePublication = anneePublication; }
+    get AnneePublication() { return this.anneePublication; }
+    set Prix(prix) { this.prix = prix; }
+    get Prix() { return this.prix; }
+}
+exports.Livre = Livre;
